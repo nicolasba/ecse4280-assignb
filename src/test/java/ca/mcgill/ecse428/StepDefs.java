fb0c1f91f5d4fe1ca1a7831dc8ecc973045313f1
Nicolas <nikba1234321@gmail.com>
2019-03-11 02:40:20 -0400

    done

--- src/test/java/ca/mcgill/ecse428/StepDefs.java
+++ src/test/java/ca/mcgill/ecse428/StepDefs.java
@@ -23,15 +23,23 @@ import java.util.List;
 
 public class StepDefs {
 
+    //Selenium
     private WebDriver driver;
     private WebDriverWait wait;
     private String driverPath = "drivers/geckodriver.exe";
-    private String url = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1552266948&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3d4db93362-4c78-4700-88b1-96478e6208c0&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015";
-    private String dummyUsername = "tempUserEcse428@outlook.com";
-    private String dummyPassword = "passwordecse428";
+    private String url = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1552266948" +
+            "&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26Rp" +
+            "sCsrfState%3d4db93362-4c78-4700-88b1-96478e6208c0&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2" +
+            "cflname%2cwld&cobrandid=90015";    //Outlook url
+
+    private Robot robot;
+
+    //Dummy data
+    private String dummyUsername = "A428test@outlook.com";
+    private String dummyPassword = "428assignment";
     private String dummySubject = "Sending funny image :) :)";
-    private String inboxWindow;
-    private String attachFileWindow;
+
+    private String recipientEmail;
 
     @Before
     public void setUp() {
@@ -41,65 +49,60 @@ public class StepDefs {
         driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 30);
 
+        //Set up robot instance
+        try{
+            robot = new Robot();
+        }
+        catch(AWTException e){
+            e.printStackTrace();
+        }
+
         //Access Outlook website and sign in using dummy account
         driver.get(url);
         driver.manage().window().maximize();
 
+        //Enter dummy username and click on next
         wait.until(ExpectedConditions.elementToBeClickable(By.id("i0116")));
         WebElement usernameInput = driver.findElement(By.id("i0116"));
         usernameInput.sendKeys(dummyUsername); //Enter username
         wait.until(ExpectedConditions.elementToBeClickable(By.id("idSIButton9")));
         driver.findElement(By.id("idSIButton9")).click();
 
-
+        //Enter dummy password and click on sign in
         wait.until(ExpectedConditions.elementToBeClickable(By.id("i0118")));
         WebElement passwordInput = driver.findElement(By.id("i0118"));
         passwordInput.sendKeys(dummyPassword); //Enter username
         wait.until(ExpectedConditions.elementToBeClickable(By.id("idSIButton9")));
         driver.findElement(By.id("idSIButton9")).click();
-//        driver.findElement(By.className("CwaK9")).click();   //Click on "Submit"
     }
 
     @Given("^I am logged into my email account$")
     public void iAmLoggedIntoMyEmailAccount() {
 
-        //Outlook
+        //Click on "New message"
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".\\_1Tegvg68M2d-4Z3rDrt2B3")));
         driver.findElement(By.cssSelector(".\\_1Tegvg68M2d-4Z3rDrt2B3")).click();
-
-        //Gmail
-//        //Click on "Compose" button
-//        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".T-I-KE")));
-//        driver.findElement(By.cssSelector(".T-I-KE")).click();
     }
 
     @And("^I enter my friend's \"([^\"]*)\"$")
-    public void iEnterMyFriendS(String email) throws Throwable {
+    public void iEnterMyFriendS(String email){
 
-        //Outlook
+        //Enter recipient email
+        recipientEmail = email;
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".ms-BasePicker-input")));
-        driver.findElement(By.cssSelector(".ms-BasePicker-input")).sendKeys(email);
-//        driver.findElement(By.cssSelector(".ms-BasePicker-input")).sendKeys("${KEY_ENTER}");
-
-        //Gmail
-//        inboxWindow = driver.getWindowHandle();
-//        //Enter recipient email in text field
-//        wait.until(ExpectedConditions.elementToBeClickable(By.name("to")));
-//        driver.findElement(By.name("to")).sendKeys(email);
+        driver.findElement(By.cssSelector(".ms-BasePicker-input")).sendKeys(recipientEmail);
     }
 
 
     @And("^I attach an \"([^\"]*)\" from my computer$")
-    public void iAttachAnFromMyComputer(String image) throws Throwable {
+    public void iAttachAnFromMyComputer(String image){
 
         //Enter image name contained under the "images" folder
         String imagePath = System.getProperty("user.dir") + File.separator + "images" + File.separator + image;
-        Robot robot = new Robot();
 
         //Click on "Attach"
         wait.until(ExpectedConditions.elementToBeClickable(By.name("Attach")));
         driver.findElement(By.name("Attach")).click();
-//        driver.findElement(By.id(":9u")).click();   //Click on "attach file" button
 
         //Click on "Browse this computer"
         wait.until(ExpectedConditions.elementToBeClickable(By.name("Browse this computer")));
@@ -113,17 +116,17 @@ public class StepDefs {
 
         //Paste from clipboard to input textfield
         robot.keyPress(KeyEvent.VK_CONTROL);
-        robot.delay(100);
+        robot.delay(300);
         robot.keyPress(KeyEvent.VK_V);
-        robot.delay(100);
+        robot.delay(300);
         robot.keyRelease(KeyEvent.VK_V);
-        robot.delay(100);
+        robot.delay(300);
         robot.keyRelease(KeyEvent.VK_CONTROL);
-        robot.delay(200);
+        robot.delay(300);
         robot.keyPress(KeyEvent.VK_ENTER);
-        robot.delay(200);
+        robot.delay(300);
         robot.keyRelease(KeyEvent.VK_ENTER);
-        robot.delay(200);
+        robot.delay(300);
     }
 
     @And("^I attach an \"([^\"]*)\" from the cloud$")
@@ -132,9 +135,8 @@ public class StepDefs {
         //Click on "Attach"
         wait.until(ExpectedConditions.elementToBeClickable(By.name("Attach")));
         driver.findElement(By.name("Attach")).click();
-//        driver.findElement(By.id(":9u")).click();   //Click on "attach file" button
 
-        //Click on "Browse this computer"
+        //Click on "Browse cloud locations"
         wait.until(ExpectedConditions.elementToBeClickable(By.name("Browse cloud locations")));
         driver.findElement(By.name("Browse cloud locations")).click();
 
@@ -146,48 +148,78 @@ public class StepDefs {
                 e.click();
         }
 
+        //Click on "Next"
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".\\_2IQpIn0UF3OF_CtC-Awq-- > .ms-Button")));
         driver.findElement(By.cssSelector(".\\_2IQpIn0UF3OF_CtC-Awq-- > .ms-Button")).click();
-
-//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[2]/div[3]/div/div/div")));
-//        WebElement textInputBar = driver.findElement(By.xpath("//div[2]/div[3]/div/div/div"));
-//        wait.until(ExpectedConditions.elementToBeClickable(textInputBar));
-//        textInputBar.click();
-
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("fe-Rg-cb")));
-//        List<WebElement> imagesNames = driver.findElements(By.className("fe-Rg-cb"));
-//        for (WebElement e : imagesNames){
-//            wait.until(ExpectedConditions.elementToBeClickable(e));
-//            if (e.getAttribute("aria-label").equals(image))
-//                e.click();
-//        }
-
-//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='picker:ap:0']")));
-//        WebElement insertBtn = driver.findElement(By.xpath("//div[@id='picker:ap:0']"));
-//        wait.until(ExpectedConditions.elementToBeClickable(insertBtn));
-//        insertBtn.click();
     }
 
     @When("^I send the email$")
     public void iSendTheEmail() throws Exception{
 
+        //Enter a dummy subject and Click on "Send"
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".ms-Button--primary")));
-        Thread.sleep(3000);
         driver.findElement(By.id("subjectLine0")).sendKeys(dummySubject);
+        Thread.sleep(3000);
         driver.findElement(By.cssSelector(".ms-Button--primary")).click();
     }
 
+    //Will return true if expectedEmail = actualEmail and expectSubject = actualSubject
+    public boolean testSubjectAndEmail () throws Exception{
 
-    @Then("^I should receive a successful confirmation$")
-    public void iShouldReceiveASuccessfulConfirmation() {
+        Thread.sleep(3000);
+        //Click on "Sent Items"
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div:nth-child(2) > div:nt" +
+                "h-child(2) > .\\_34_bqC0c1-8H3B0lGzop-9:nth-child(1) > .\\_2qZaU4w9P1XG8-zs5arlR3")));
+        driver.findElement(By.cssSelector("div:nth-child(2) > div:nth-child(2) > .\\_34_bqC0c1-" +
+                "8H3B0lGzop-9:nth-child(1) > .\\_2qZaU4w9P1XG8-zs5arlR3")).click();
 
-        //Gmail
-//        Assert.assertTrue(wait.until(ExpectedConditions.textToBe(By.className("bAq"), "Message sent.")));
+        //Press on "Down Arrow" key to select first email in list
+        robot.keyPress(KeyEvent.VK_DOWN);
+        robot.delay(100);
+        robot.keyRelease(KeyEvent.VK_DOWN);
+        robot.delay(100);
+
+        //Get subject and recipient email
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("dJ4kO5HcLAM4x-GXmxP8n")));
+        String actualSubject = driver.findElement(By.className("dJ4kO5HcLAM4x-GXmxP8n")).getText();
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("_1GbKnlrcyAfdgFr9WpTgdU")));
+        String actualEmail = driver.findElement(By.className("_1GbKnlrcyAfdgFr9WpTgdU")).getText();
+
+        return actualSubject.equals(dummySubject) && actualEmail.equals(recipientEmail);
+    }
+
+    @Then("^I should be able to verify that the email was successfully sent$")
+    public void iShouldBeAbleToVerifyThatTheEmailWasSuccessfullySent() throws Exception{
+
+        //Assert that expected/actual email and subject are the same
+        Assert.assertTrue(testSubjectAndEmail());
+    }
+
+    public boolean isWarningPresent() throws Exception{
+
+        Thread.sleep(3000);
+        //Get warning
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("_2eDT5LAxGsFuAUVDuX3mz_ _3zUVDEaVDhyHmlV2" +
+                "830f9z F99zb1PyC88tXPxMniJJF")));
+        String warning = driver.findElement(By.className("_2eDT5LAxGsFuAUVDuX3mz_ _3zUVDEaVDhyHmlV2830f" +
+                "9z F99zb1PyC88tXPxMniJJF")).getText();
+
+        return warning.equals("This message must have at least one recipient.");
+    }
+
+    @Then("^I should be warned that the email could not be sent$")
+    public void iShouldBeWarnedThatTheEmailCouldNotBeSent() throws Exception {
+
+        Assert.assertTrue(isWarningPresent());
     }
 
     @After
     public void tearDown() {
 
-//        driver.close();
+        //Sign out
+
+
+        //Terminate Web driver
+        driver.close();
     }
 }
--- src/test/resource/sendEmailWithImage.feature
+++ src/test/resource/sendEmailWithImage.feature
@@ -1,5 +1,7 @@
 Feature: Send an email with an image attachment
-  As a user with a valid email account, I would like to be able to send an email containing an image attachment to my friend.
+  As an Outlook user with a valid email account
+  I would like to send an email with an image attachment
+  So that the recipient I send it to can see it.
 
 #  Scenario Outline: Email sent with an image stored locally (Normal flow)
 #
@@ -7,38 +9,44 @@ Feature: Send an email with an image attachment
 #    And I enter my friend's "<email>"
 #    And I attach an "<image>" from my computer
 #    When I send the email
-#    Then I should receive a successful confirmation
+#    Then I should be able to verify that the email was successfully sent
 #
 #    Examples:
 #      |                email                |     image        |
 #      | jose.barreyrolopez@mail.mcgill.ca   |   confused.jpg   |
-##      | nicobarreyro@hotmail.com            |   confused.jpg   |
-
-  Scenario Outline:  Email sent with an image that is on the Google Drive (Alternate flow)
-
-    Given I am logged into my email account
-    And I enter my friend's "<email>"
-    And I attach an "<image>" from the cloud
-    When I send the email
-    Then I should receive a successful confirmation
-
-    Examples:
-      |                email                |      image       |
-#      | jose.barreyrolopez@mail.mcgill.ca   |     confused.jpg       |
-      | nicobarreyro@hotmail.com            |     shaking_toad.gif       |
-
+#      | nicobarreyro@hotmail.com            |   confused.jpg   |
+#      | nicobarreyro@hotmail.com            |   train.jpg      |
+#      | erion.hysa@mail.mcgill.ca           |   flags.jpg      |
+#      | nicobarreyro@hotmail.com            |   office.jpg     |
 #
-#  Scenario Outline: Error Email sent with an image attachment (Error flow)
+#  Scenario Outline: Email sent with an image that is on the cloud (OneDrive) (Alternate flow)
 #
 #    Given I am logged into my email account
 #    And I enter my friend's "<email>"
-#    And I attach an "<image>"
+#    And I attach an "<image>" from the cloud
 #    When I send the email
-#    Then I should receive a successful confirmation
+#    Then I should be able to verify that the email was successfully sent
 #
 #    Examples:
-#      |                email                |     image      |
-#      | jose.barreyrolopez@mail.mcgill.ca   |   asdada   |
-##      | nicobarreyro@hotmail.com            |   asdada    |
+#      |                email                |     image        |
+#      | jose.barreyrolopez@mail.mcgill.ca   |   confused.jpg   |
+#      | nicobarreyro@hotmail.com            |   confused.jpg   |
+#      | nicobarreyro@hotmail.com            |   train.jpg      |
+#      | erion.hysa@mail.mcgill.ca           |   flags.jpg      |
+#      | nicobarreyro@hotmail.com            |   office.jpg     |
+
+#
+  Scenario Outline: Email sent without a recipient email (Error flow)
+
+    Given I am logged into my email account
+    And I attach an "<image>" from my computer
+    When I send the email
+    Then I should be warned that the email could not be sent
+
+    Examples:
+      |     image        |
+      |   confused.jpg   |
+      |   flags.jpg      |
+      |   office.jpg     |
 
 
